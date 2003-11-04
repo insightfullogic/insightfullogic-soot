insightfullogic/insightfullogic-soot

commit 7b35f3ed4479924def6db7a60ae57a4d65d2eb49

spark dumps (pag, pag.jar, solution, types) are now written to the output directory (-d dir) rather than to the current directory this is important for the soot plugin, because the files were being written to the directory eclipse was started in, rather than the project/sootOutput directory
git-svn-id: https://svn.sable.mcgill.ca/soot/soot/trunk@1157 8cfe803f-38b8-0310-ad8c-c0bb4caa2ed3

src/soot/jimple/spark/SparkTransformer.java
@@ -28,6 +28,7 @@ import soot.jimple.*;
 import java.util.*;
 import soot.util.*;
 import soot.options.SparkOptions;
+import soot.options.Options;
 import soot.tagkit.*;
 
 /** Main entry point for Spark.
@@ -41,6 +42,7 @@ public class SparkTransformer extends AbstractSparkTransformer
     protected void internalTransform( String phaseName, Map options )
     {
         SparkOptions opts = new SparkOptions( options );
+        final String output_dir = Options.v().output_dir();
 
         // Build pointer assignment graph
         ContextInsensitiveBuilder b = new ContextInsensitiveBuilder();
@@ -88,7 +90,7 @@ public class SparkTransformer extends AbstractSparkTransformer
         // Dump pag
         PAGDumper dumper = null;
         if( opts.dump_pag() || opts.dump_solution() ) {
-            dumper = new PAGDumper( pag );
+            dumper = new PAGDumper( pag, output_dir );
         }
         if( opts.dump_pag() ) dumper.dump();
 
@@ -142,9 +144,9 @@ public class SparkTransformer extends AbstractSparkTransformer
         }
         */
 
-        if( opts.dump_answer() ) new ReachingTypeDumper( pag ).dump();
+        if( opts.dump_answer() ) new ReachingTypeDumper( pag, output_dir ).dump();
         if( opts.dump_solution() ) dumper.dumpPointsToSets();
-        if( opts.dump_html() ) new PAG2HTML( pag ).dump();
+        if( opts.dump_html() ) new PAG2HTML( pag, output_dir ).dump();
         Scene.v().setPointsToAnalysis( pag );
         if( opts.add_tags() ) {
             addTags( pag );

src/soot/jimple/ReachingTypeDumper.java
@@ -28,13 +28,14 @@ import java.io.*;
  * @author Ondrej Lhotak
  */
 public class ReachingTypeDumper {
-    public ReachingTypeDumper( PointsToAnalysis pa ) {
+    public ReachingTypeDumper( PointsToAnalysis pa, String output_dir ) {
         this.pa = pa;
+        this.output_dir = output_dir;
     }
     public void dump() {
         try {
             PrintWriter file = new PrintWriter(
-                new FileOutputStream( "types" ) );
+                new FileOutputStream( new File(output_dir, "types") ) );
             for( Iterator it = Scene.v().getApplicationClasses().iterator();
                     it.hasNext(); ) {
                 handleClass( file, (SootClass) it.next() );
@@ -54,6 +55,7 @@ public class ReachingTypeDumper {
     /* End of package methods. */
 
     protected PointsToAnalysis pa;
+    protected String output_dir;
 
     protected void handleClass( PrintWriter out, SootClass c ) {
         for( Iterator mIt = c.methodIterator(); mIt.hasNext(); ) {

src/soot/jimple/spark/pag/PAG2HTML.java
@@ -31,8 +31,9 @@ import java.util.zip.*;
  * @author Ondrej Lhotak
  */
 public class PAG2HTML {
-    public PAG2HTML( PAG pag ) {
+    public PAG2HTML( PAG pag, String output_dir ) {
         this.pag = pag;
+        this.output_dir = output_dir;
     }
     public void dump() {
         for( Iterator vIt = pag.getVarNodeNumberer().iterator(); vIt.hasNext(); ) {
@@ -44,7 +45,7 @@ public class PAG2HTML {
         }
         try {
             JarOutputStream jarOut = new JarOutputStream(
-                    new FileOutputStream( "pag.jar" ) );
+                    new FileOutputStream( new File(output_dir, "pag.jar") ) );
             for( Iterator vIt = mergedNodes.keySet().iterator(); vIt.hasNext(); ) {
                 final VarNode v = (VarNode) vIt.next();
                 dumpVarNode( v, jarOut );
@@ -65,6 +66,7 @@ public class PAG2HTML {
     /* End of package methods. */
 
     protected PAG pag;
+    protected String output_dir;
     protected MultiMap mergedNodes = new HashMultiMap();
     protected MultiMap methodToNodes = new HashMultiMap();
 

src/soot/jimple/spark/pag/PAGDumper.java
@@ -30,13 +30,14 @@ import soot.jimple.spark.sets.PointsToSetInternal;
  * @author Ondrej Lhotak
  */
 public class PAGDumper {
-    public PAGDumper( PAG pag ) {
+    public PAGDumper( PAG pag , String output_dir ) {
         this.pag = pag;
+        this.output_dir = output_dir;
     }
     public void dumpPointsToSets() {
         try {
             final PrintWriter file = new PrintWriter(
-                    new FileOutputStream( "solution" ) );
+                    new FileOutputStream( new File(output_dir, "solution") ) );
             file.println( "Solution:" );
             for( Iterator vnIt = pag.getVarNodeNumberer().iterator(); vnIt.hasNext(); ) {
                 final VarNode vn = (VarNode) vnIt.next();
@@ -66,7 +67,7 @@ public class PAGDumper {
     public void dump() {
         try {
             PrintWriter file = new PrintWriter(
-                new FileOutputStream( "pag" ) );
+                new FileOutputStream( new File(output_dir, "pag") ) );
 
             if( pag.getOpts().topo_sort() ) {
                 new TopoSorter( pag, false ).sort();
@@ -134,6 +135,7 @@ public class PAGDumper {
     /* End of package methods. */
 
     protected PAG pag;
+    protected String output_dir;
     protected int fieldNum = 0;
     protected HashMap fieldMap = new HashMap();
     protected ObjectNumberer root = new ObjectNumberer( null, 0 );

commit 579bcce6848b3b3f2df00b336a5d5234baa14157

Fix bug introduced in r1157: when no output dir is specified, Soot should put the various output files from Spark into the default output dir sootOutput, rather than the root directory as r1157 made it do.
git-svn-id: https://svn.sable.mcgill.ca/soot/soot/trunk@1287 8cfe803f-38b8-0310-ad8c-c0bb4caa2ed3

src/soot/jimple/spark/SparkTransformer.java
@@ -42,7 +42,7 @@ public class SparkTransformer extends AbstractSparkTransformer
     protected void internalTransform( String phaseName, Map options )
     {
         SparkOptions opts = new SparkOptions( options );
-        final String output_dir = Options.v().output_dir();
+        final String output_dir = SourceLocator.v().getOutputDir();
 
         // Build pointer assignment graph
         ContextInsensitiveBuilder b = new ContextInsensitiveBuilder();