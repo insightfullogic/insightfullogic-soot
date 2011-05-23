f7fe55fa5cd20534db2dff35ee92a743857c4644
John Oliver <johno@insightfullogic.com>
2011-05-23 13:13:13 +0100

    Add maven deploy script

---
+++ deployJar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mvn deploy:deploy-file -DgroupId=soot -DartifactId=soot -Dversion=trunk -Dpackaging=jar -Dfile=./lib/sootclasses-trunk.jar -Durl=http://10.8.0.1:6397/nexus-webapp-1.9/content/repositories/thirdparty/ -DrepositoryId=nexus
