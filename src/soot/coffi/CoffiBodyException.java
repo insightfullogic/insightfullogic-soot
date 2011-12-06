cdc6e29ce33b93ac101f7a8848f8c90a4fe039bb
John Oliver <johno@insightfullogic.com>
2011-12-06 17:08:41 +0000

    Add missing file

---
+++ src/soot/coffi/CoffiBodyException.java
@@ -0,0 +1,9 @@
+package soot.coffi;
+
+public class CoffiBodyException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public CoffiBodyException(String string) {
+		super(string);
+	}
+}
